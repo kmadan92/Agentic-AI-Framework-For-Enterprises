5e762987a34b61c51fd6509385dd33757ae27da6
Kapil Madan <kapilmadan@microsoft.com>
2026-03-18 15:11:12 +0530

    solving args inout elicitation using prompt engineering

--- server.py
+++ server.py
@@ -1,24 +1,24 @@
 from mcp.server.fastmcp import FastMCP
-from langgraph.types import Command
-from backend.hitl import setup_async_graph
-import uuid
 
 # Initialize your MCP Server
 mcp = FastMCP("LLM App")
 
 @mcp.tool()
-async def run_node_test(node_id: str, test_type: str) -> str:
+async def run_node_test(node_id: [str], test_type: str) -> str:
     """
-    Executes a probation test on a server node.
+    Executes a probation test on a server node for given NodeId or NodeIds and Test Type.
     
     CRITICAL INSTRUCTIONS FOR AI:
-    You MUST have both a 'node_id' and a 'test_type' (e.g., CPU, Memory, Network) to call this tool.
-    If the user asks to run a test but does NOT provide these details, DO NOT CALL THIS TOOL. 
-    Instead, stop and ask the user to provide the missing 'node_id' and 'test_type'.
+    You MUST have both node_id' or  list of node ids and a 'test_type' (e.g., CPU, Memory, Network) to call this tool.
+    
+    If you are missing any of these details, DO NOT CALL THIS TOOL. Instead, you must collect the missing information EXACTLY ONE QUESTION AT A TIME:
+    1. First, check if you have the 'node_id'. If you do not, ask the user ONLY for the Node ID or list of Node IDs and stop. Wait for their reply.
+    2. Once you have the 'node_id', check if you have the 'test_type'. If you do not, ask the user ONLY for the test type and stop. Wait for their reply.
+    3. NEVER ask for both the Node ID and the Test Type in the same message.
     """
     
     return "Tests failed"
 
-# Run the MCP server (typically handled by your specific MCP runner/transport)
+# Run the MCP server
 if __name__ == "__main__":
     mcp.run()
